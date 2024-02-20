1c411385dffe9572afd0a51e67bc109e61cfc72a
gokturkturan <113911139+gokturkturan@users.noreply.github.com>
2024-02-21 00:06:00 +0300

    NextRoom Frontend

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "./head";
+import { GlobalProvider } from "./GlobalProvider";
+import Script from "next/script";
+import "bootstrap/dist/css/bootstrap.css";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +22,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head />
+      <body className={inter.className}>
+        <GlobalProvider>
+          <Header />
+          {children}
+          <Footer />
+        </GlobalProvider>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></Script>
+        <Script src="https://kit.fontawesome.com/9edb65c86a.js"></Script>
+      </body>
     </html>
   );
 }
